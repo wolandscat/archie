0545739d71e65c2400714f061a5d6f6290ff7118
Stefan Spiska <s.spiska@symeda.de>
2019-05-23 14:52:32 +0200

    EHR-237: added value equals and constructors (directory)

--- openehr-rm/src/main/java/com/nedap/archie/rm/directory/Folder.java
+++ openehr-rm/src/main/java/com/nedap/archie/rm/directory/Folder.java
@@ -1,12 +1,14 @@
 package com.nedap.archie.rm.directory;
 
-import com.nedap.archie.rm.archetyped.Locatable;
+import com.nedap.archie.rm.archetyped.*;
+import com.nedap.archie.rm.datavalues.DvText;
 import com.nedap.archie.rm.support.identification.ObjectRef;
+import com.nedap.archie.rm.support.identification.UIDBasedId;
+
 import javax.annotation.Nullable;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlType;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,6 +25,21 @@ public class Folder extends Locatable {
     private List<ObjectRef> items = new ArrayList<>();
     @Nullable
     private List<Folder> folders = new ArrayList<>();
+
+    public Folder() {
+    }
+
+    public Folder(String archetypeNodeId, DvText name, @Nullable List<ObjectRef> items, @Nullable List<Folder> folders) {
+        super(archetypeNodeId, name);
+        this.items = items;
+        this.folders = folders;
+    }
+
+    public Folder(@Nullable UIDBasedId uid, String archetypeNodeId, DvText name, @Nullable Archetyped archetypeDetails, @Nullable FeederAudit feederAudit, @Nullable List<Link> links, @Nullable Pathable parent, @Nullable String parentAttributeName, @Nullable List<ObjectRef> items, @Nullable List<Folder> folders) {
+        super(uid, archetypeNodeId, name, archetypeDetails, feederAudit, links, parent, parentAttributeName);
+        this.items = items;
+        this.folders = folders;
+    }
 
     @Nullable
     public List<ObjectRef> getItems() {
@@ -50,5 +67,26 @@ public class Folder extends Locatable {
     public void addFolder(Folder folder) {
         this.folders.add(folder);
         this.setThisAsParent(folder, "folders");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        Folder folder = (Folder) o;
+
+        if (items != null ? !items.equals(folder.items) : folder.items != null) return false;
+        return folders != null ? folders.equals(folder.folders) : folder.folders == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (items != null ? items.hashCode() : 0);
+        result = 31 * result + (folders != null ? folders.hashCode() : 0);
+        return result;
     }
 }
