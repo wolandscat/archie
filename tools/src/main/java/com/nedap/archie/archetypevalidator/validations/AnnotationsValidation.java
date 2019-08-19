92b46f75f1497959dd02e51e4663ab615d09081a
BWHoeben <bart.hoeben@nedap.com>
2019-08-19 14:04:47 +0200

    Fix bug

--- tools/src/main/java/com/nedap/archie/archetypevalidator/validations/AnnotationsValidation.java
+++ tools/src/main/java/com/nedap/archie/archetypevalidator/validations/AnnotationsValidation.java
@@ -1,11 +1,13 @@
 package com.nedap.archie.archetypevalidator.validations;
 
 
+import com.nedap.archie.aom.Archetype;
 import com.nedap.archie.aom.AuthoredArchetype;
 import com.nedap.archie.aom.ResourceAnnotations;
 import com.nedap.archie.aom.utils.AOMUtils;
 import com.nedap.archie.archetypevalidator.ArchetypeValidationBase;
 import com.nedap.archie.archetypevalidator.ErrorType;
+import com.nedap.archie.query.AOMPathQuery;
 import org.openehr.utils.message.I18n;
 
 import java.util.Map;
@@ -26,10 +28,10 @@ public class AnnotationsValidation extends ArchetypeValidationBase {
                 for(String language: annotations.getDocumentation().keySet()) {
                     Map<String, Map<String, String>> annotationsForLanguage = annotations.getDocumentation().get(language);
                     for(String path: annotationsForLanguage.keySet()) {
-                        Map<String, String> annotationForPath = annotationsForLanguage.get(path);
+                        Map<String, String> annotationsForPath = annotationsForLanguage.get(path);
                         boolean isArchetypePath = AOMUtils.isArchetypePath(path) ; //TODO: NO idea what the eiffel code here suggests it does
                         if(isArchetypePath) {
-                            if(!archetype.hasPath(path) || (flatParent != null && !flatParent.hasPath(path))) {
+                            if(!(hasPath(path, archetype) || (flatParent != null && hasPath(path, flatParent)))) {
                                 addMessage(ErrorType.VRANP, I18n.t("The path {0} referenced in the annotations does not exist in the flat archetype", path));
                             }
                         } else { //TODO: this can also be referencemodel.has_path, but that's not implemented yet
@@ -45,5 +47,15 @@ public class AnnotationsValidation extends ArchetypeValidationBase {
         }
 
 
+    }
+
+    /**
+     * Check if the archetype has this path directly, or a specialized variant of this path
+     * @param path the path to check
+     * @param archetype the archetype to find the path in
+     * @return true if the archetype has the path, false if it does not
+     */
+    private boolean hasPath(String path, Archetype archetype) {
+        return ! new AOMPathQuery(path).findList(archetype.getDefinition(), true).isEmpty();
     }
 }
