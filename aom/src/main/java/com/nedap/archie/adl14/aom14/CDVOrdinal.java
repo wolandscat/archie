b28dbd2783475dd2e2880b617a3b9d548b8450ad
Pieter Bos <pieter.bos@nedap.com>
2019-10-07 15:56:24 +0200

    Add C_DV_ORDINAL support in ODIN form

---
+++ aom/src/main/java/com/nedap/archie/adl14/aom14/CDVOrdinal.java
@@ -0,0 +1,17 @@
+package com.nedap.archie.adl14.aom14;
+
+import java.util.List;
+import java.util.Map;
+
+public class CDVOrdinal {
+
+    private Map<String, CDVOrdinalItem> list;
+
+    public Map<String, CDVOrdinalItem> getList() {
+        return list;
+    }
+
+    public void setList(Map<String, CDVOrdinalItem> list) {
+        this.list = list;
+    }
+}
