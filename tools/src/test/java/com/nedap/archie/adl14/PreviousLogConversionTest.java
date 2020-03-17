182442ad751cb79d0acefc35ca25ba84e17e1aba
Pieter Bos <pieter.bos@nedap.com>
2020-03-17 10:36:25 +0100

    Add value set terminology code even when it comes from the conversion log in adl 1.4 converter

--- tools/src/test/java/com/nedap/archie/adl14/PreviousLogConversionTest.java
+++ tools/src/test/java/com/nedap/archie/adl14/PreviousLogConversionTest.java
@@ -3,14 +3,19 @@ package com.nedap.archie.adl14;
 import com.google.common.collect.Lists;
 import com.nedap.archie.adl14.log.ADL2ConversionLog;
 import com.nedap.archie.adl14.log.ADL2ConversionRunLog;
+import com.nedap.archie.aom.Archetype;
 import com.nedap.archie.aom.CAttribute;
 import com.nedap.archie.aom.CObject;
+import com.nedap.archie.archetypevalidator.ArchetypeValidator;
+import com.nedap.archie.archetypevalidator.ValidationResult;
+import com.nedap.archie.serializer.adl.ADLArchetypeSerializer;
 import org.junit.Test;
 import org.openehr.referencemodels.BuiltinReferenceModels;
 
 import java.io.InputStream;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class PreviousLogConversionTest {
 
@@ -44,4 +49,43 @@ public class PreviousLogConversionTest {
 
         }
     }
+
+    @Test
+    public void testValueSet() throws Exception {
+        ADL14ConversionConfiguration conversionConfiguration = ConversionConfigForTest.getConfig();
+        ADL14Converter converter = new ADL14Converter(BuiltinReferenceModels.getMetaModels(), conversionConfiguration);
+        ADL2ConversionRunLog log = null;
+
+        try(InputStream stream = getClass().getResourceAsStream("openehr-EHR-OBSERVATION.respiration.v1.adl")) {
+            ADL2ConversionResultList result = converter.convert(
+                    Lists.newArrayList(new ADL14Parser(BuiltinReferenceModels.getMetaModels()).parse(stream, conversionConfiguration)));
+            log = result.getConversionLog();
+            Archetype converted = result.getConversionResults().get(0).getArchetype();
+            ValidationResult validated = new ArchetypeValidator(BuiltinReferenceModels.getMetaModels()).validate(converted);
+            assertTrue(validated.toString(), validated.passes() );
+            System.out.println(ADLArchetypeSerializer.serialize(result.getConversionResults().get(0).getArchetype()));
+            assertTrue(converted.getTerminology().getTermDefinitions().get("nl").containsKey("ac9000"));
+            assertTrue(converted.getTerminology().getTermDefinitions().get("nl").containsKey("ac9001"));
+            assertTrue(converted.getTerminology().getTermDefinitions().get("nl").containsKey("ac9002"));
+            assertTrue(converted.getTerminology().getTermDefinitions().get("nl").containsKey("ac9003"));
+        }
+
+        assertEquals(1, log.getConvertedArchetypes().size());
+
+        try(InputStream stream = getClass().getResourceAsStream("openehr-EHR-OBSERVATION.respiration.v1.adl")) {
+            ADL2ConversionResultList result = converter.convert(
+                    Lists.newArrayList(new ADL14Parser(BuiltinReferenceModels.getMetaModels()).parse(stream, conversionConfiguration)),
+                    log);
+            Archetype converted = result.getConversionResults().get(0).getArchetype();
+            System.out.println(ADLArchetypeSerializer.serialize(result.getConversionResults().get(0).getArchetype()));
+            ValidationResult validated = new ArchetypeValidator(BuiltinReferenceModels.getMetaModels()).validate(converted);
+            assertTrue(validated.toString(), validated.passes() );
+            assertTrue(converted.getTerminology().getTermDefinitions().get("nl").containsKey("ac9000"));
+            assertTrue(converted.getTerminology().getTermDefinitions().get("nl").containsKey("ac9001"));
+            assertTrue(converted.getTerminology().getTermDefinitions().get("nl").containsKey("ac9002"));
+            assertTrue(converted.getTerminology().getTermDefinitions().get("nl").containsKey("ac9003"));
+
+        }
+    }
+
 }
