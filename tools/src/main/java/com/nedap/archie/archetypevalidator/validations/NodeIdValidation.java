34335f83c7a444a0e1bae3d6b91333fb37399ce8
Pieter Bos <pieter.bos@nedap.com>
2020-03-24 16:40:19 +0100

    Fix CBoolean validation, allow duplicate node ids on changing occurrences to {0} (yes really)

--- tools/src/main/java/com/nedap/archie/archetypevalidator/validations/NodeIdValidation.java
+++ tools/src/main/java/com/nedap/archie/archetypevalidator/validations/NodeIdValidation.java
@@ -34,8 +34,8 @@ public class NodeIdValidation extends ValidatingVisitor {
             addMessageWithPath(ErrorType.VCOID, cObject.getPath(),
                     I18n.t("C_OBJECT with RM type {0} must have a node id", cObject.getRmTypeName()));
         }
-        else if(!CPrimitiveObject.PRIMITIVE_NODE_ID_VALUE.equals(cObject.getNodeId()) && nodeIds.containsKey(cObject.getNodeId())) {
-            //every node id in a single archetype must be unique or a primitive object
+        else if(!CPrimitiveObject.PRIMITIVE_NODE_ID_VALUE.equals(cObject.getNodeId()) && !(cObject.getOccurrences() != null && cObject.getOccurrences().isProhibited()) && nodeIds.containsKey(cObject.getNodeId())) {
+            //every node id in a single archetype must be unique or a primitive object, or a occurrences matches {0} because sometimes that's the only way
             addMessageWithPath(ErrorType.VCOSU, cObject.getPath(), I18n.t("Node id {0} already used in path {1}", cObject.getNodeId(), nodeIds.get(cObject.getNodeId())));
         }
         nodeIds.put(cObject.getNodeId(), cObject.getPath());
